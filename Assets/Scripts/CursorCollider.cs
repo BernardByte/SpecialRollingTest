c7f53a7ed3c5cd35d3919519098e886e2eed42f0
Muhammad imran <96988844+BernardByte@users.noreply.github.com>
2024-07-03 12:27:16 +0800

    Trying to show an arc
    When user select a button, an arc will be shown to visualize how user have to make a semi-circle.

--- Assets/Scripts/CursorCollider.cs
+++ Assets/Scripts/CursorCollider.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
+
 public class CursorCollider : MonoBehaviour
 {
     private Hive hiveScript;
@@ -7,9 +9,13 @@ public class CursorCollider : MonoBehaviour
     private Color selectedColor = new Color(0.055f, 0.561f, 0.243f);
     private Color originalColor = new Color(0.08891062f, 0.1522242f, 0.1698113f);
 
+    public GameObject arc;
+    public GameObject visualizeArc;
+
     private void Start()
     {
         hiveScript = FindObjectOfType<Hive>();
+
     }
 
 
@@ -19,12 +25,22 @@ public class CursorCollider : MonoBehaviour
         if (other.gameObject.tag == "AlphabetKey" || other.gameObject.tag == "Space" || other.gameObject.tag == "Enter" || other.gameObject.tag == "BackSpace" || other.gameObject.tag == "Shift")
         {
             hiveScript.selectedButtonR = other.gameObject;
-           
+            //visualizeArc = Instantiate(arc, hiveScript.selectedButtonR.transform.position, Quaternion.identity);
+            //visualizeArc.transform.localScale = new Vector3(0.007f,0.007f,0);
+            //visualizeArc.transform.SetParent(hiveScript.selectedButtonR.transform, true);
+            //if (other.gameObject.transform.Find("Arc").gameObject)
+            //{
+            //    visualizeArc = hiveScript.selectedButtonR.transform.Find("Arc").gameObject;
+            //    visualizeArc.SetActive(true);
+
+            //}
+
+
         }
         else if (other.gameObject.tag == "AlphabetKeyL" || other.gameObject.tag == "SpaceL" || other.gameObject.tag == "NumKeyL" || other.gameObject.tag == "ABC" || other.gameObject.tag == "StartL")
         {
             hiveScript.selectedButtonL = other.gameObject;
-            
+
         }
     }
 
@@ -34,7 +50,11 @@ public class CursorCollider : MonoBehaviour
         if (other.gameObject == hiveScript.selectedButtonR)
         {
             hiveScript.selectedButtonR = null;
-             hiveScript.ResetCurveRelatedData("RightSide");
+            hiveScript.ResetCurveRelatedData("RightSide");
+            //if(visualizeArc != null)
+            //{
+            //visualizeArc.SetActive(false);
+            //}
         }
         else if (other.gameObject == hiveScript.selectedButtonL)
         {
